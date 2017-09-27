d6a8630089b8262cad98b0124209107f0ed58b61
Danielv123 <tranen.bananen@gmail.com>
2017-09-27 21:35:01 +0200

    Added inline documentation with documentation.js

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+/** 
+In memory timeseries database with averaged/summed values to reduce performance footprint
+@author       Daniel Vestøl
 
+@constructor
+@param {object} options Options object, can be left as blank to use defaults
+@param {function} [log] Logging function, can be left as undefined for no logging
+@returns {object} timeSeries
+*/
 
 module.exports = function(options, log){
 	if(log) log("Logging enabled!");
@@ -24,13 +32,27 @@ module.exports = function(options, log){
 	} else {
 		this.data = {};
 	}
-	
-	function getCurrentIndex(date){
+	/**
+	* Get current index in datastore array
+	* @private
+	* @param {number} date Unix epoch time in ms returned from Date.now();
+	* @returns {number} index Current index in this.data
+	*/
+	this.getCurrentIndex = function(date){
 		return Math.floor( date * options.entriesPerSecond / 1000 ) % options.maxEntries;
 	}
-	function getOriginIndex(x){
-		return (Math.floor(Date.now() * options.entriesPerSecond / 1000) - Math.floor(x * options.entriesPerSecond / 1000)) % options.maxEntries
-	}
+	/**
+	 * Adds timeseries data to a collection
+	 * 
+	 * @example
+	 * let series = new timeSeries();
+	 * series.add({
+	 *   key: "apples",
+	 *   value: 3,
+	 * });
+	 * 
+	 * @param {object} entry
+	*/
 	this.add = function(entry){
 		/* {
 			key: "string",
@@ -64,6 +86,14 @@ module.exports = function(options, log){
 			}
 		}
 	}
+	/**
+	 * Retrieves timeseries data
+	 * @public
+	 * @param {number} datapoints the number of datapoints to retrieve, has to be less than size of collection
+	 * @param {string} key name of collection to retrieve
+	 * 
+	 * @returns {array} of objects like [{key: "string", value: number}]
+	*/
 	this.get = function(datapoints, key){ // to defaults to Date.now
 		if(typeof datapoints != "number"){
 			throw "ERROR: Argument 1 is not a number!";
@@ -88,6 +118,9 @@ module.exports = function(options, log){
 		}
 		return readyObject;
 	}
+	/**
+	 * Clears all data
+	*/
 	this.clear = function(){
 		this.data = [];
 	}
